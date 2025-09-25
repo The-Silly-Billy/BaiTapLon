49be1a3eabe899c94ccebb1893ea7a08c0896197
Silly Billy <thesillybilly1101@gmail.com>
2025-09-25 21:56:49 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
 
+        //Xem nta day cach dung SWING JAVAX trong video nay nhe =)))) May cai t code cx la tu video nay ra day
+        //https://www.youtube.com/watch?v=om59cwR7psI&list=PL_QPQmz5C6WUF-pOQDsbsKbaBZqXj4qSq&index=1&ab_channel=RyiSnow
+        
         //khoi tao window
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
