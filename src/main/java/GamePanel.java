a55449adb1aa3af35039daf7f9b0e3b4112f43c8
Silly Billy <thesillybilly1101@gmail.com>
2025-09-25 21:53:00 +0700

    hihi

---
+++ src/main/java/GamePanel.java
@@ -0,0 +1,81 @@
+import GameObject.GameObject;
+
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    //Cai dat pixel
+    final int originalTileSize = 16;                        // obj se trong 1 khung pixel 16x16
+    final int scale = 3;                                    // phong dai obj len 3 lan
+    final int tileSize = originalTileSize * scale;          // 48 pixel
+
+    // man hinh ti le 4:3
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+
+    final int screenWidth = tileSize * maxScreenCol;        //768 pixels
+    final int screenHeight = tileSize * maxScreenRow;       //576 pixels
+
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000.0 / FPS;      // tgian ve 1 khung hinh (0.0166s)
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while(gameThread != null) {
+
+            //Setup FPS dua tren "DELTA METHOD"
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+//                update();
+                repaint();
+                delta--;
+            }
+
+        }
+    }
+
+    public void udpate() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(100,100, tileSize, tileSize);
+
+        g2.dispose();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
+
+        //khoi tao window
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("A R K A N O I D");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
 
     }
 }
