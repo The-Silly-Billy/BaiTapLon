ab3bf7379f6d952520dc06606af1e9fb9098a4c1
ipresstheEsc <82923961+RenisDo@users.noreply.github.com>
2025-10-17 00:21:33 +0700

    Ver3.5: implement keyboard navigation in StartMenu

--- src/main/java/Main/KeyHandler.java
+++ src/main/java/Main/KeyHandler.java
@@ -2,26 +2,35 @@ package Main;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import GameUI.GameState;
 
 public class KeyHandler implements KeyListener{
-
+    private GamePanel gamePanel;
     public boolean leftPressed, rightPressed, spacePressed;
 
+    public KeyHandler(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
     @Override
     public void keyTyped(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
-        int code = e.getKeyCode();
 
-        if(code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
-            leftPressed = true;
-        }
-        if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
-            rightPressed = true;
-        }
-        if(code == KeyEvent.VK_SPACE) {
-            spacePressed = true;
+        if(gamePanel.getState() == GameState.MENU) {
+            gamePanel.getMenu().keyPressed(e);
+        } else if (gamePanel.getState() == GameState.PLAYING) {
+            int code = e.getKeyCode();
+
+            if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
+                leftPressed = true;
+            }
+            if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
+                rightPressed = true;
+            }
+            if (code == KeyEvent.VK_SPACE) {
+                spacePressed = true;
+            }
         }
     }
 
